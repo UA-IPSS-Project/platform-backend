e331351feddb513ce3f390cd57dc54ca95e74957
Danuny21 <daniel.f.nunes@ua.pt>
2026-02-28 19:14:56 +0000

    feat: Initialize requisicoes module with Maven configuration, application setup, and PostgreSQL service

---
+++ requisicoes/src/main/java/pt/florinhas/requisicoes/RequisicoesApplication.java
@@ -0,0 +1,13 @@
+package pt.florinhas.requisicoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequisicoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequisicoesApplication.class, args);
+	}
+
+}
