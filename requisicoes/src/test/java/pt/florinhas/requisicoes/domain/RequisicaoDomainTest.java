f993ec3f0f7e8146e15f3eec2da04568e133afb1
Ana Pereira <ana.c.pereira@ua.pt>
2026-03-16 17:49:26 +0000

    refactor: Remove unnecessary blank line in RequisicaoDomainTest for improved readability

--- requisicoes/src/test/java/pt/florinhas/requisicoes/domain/RequisicaoDomainTest.java
+++ requisicoes/src/test/java/pt/florinhas/requisicoes/domain/RequisicaoDomainTest.java
@@ -2,7 +2,6 @@ package pt.florinhas.requisicoes.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import org.junit.jupiter.api.Test;
 
 class RequisicaoDomainTest {
