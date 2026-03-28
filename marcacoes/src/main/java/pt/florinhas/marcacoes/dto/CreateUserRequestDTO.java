4b2d2cc1970df2c475e32cb7b5e968a8f4f534ff
Danuny21 <daniel.f.nunes@ua.pt>
2026-03-28 15:38:27 +0000

    refactor: make contact fields optional in user registration DTOs

--- marcacoes/src/main/java/pt/florinhas/marcacoes/dto/CreateUserRequestDTO.java
+++ marcacoes/src/main/java/pt/florinhas/marcacoes/dto/CreateUserRequestDTO.java
@@ -16,7 +16,6 @@ public class CreateUserRequestDTO {
     @Pattern(regexp = "\\d{9}", message = "NIF deve conter exatamente 9 dígitos numéricos")
     private String nif;
 
-    @NotBlank(message = "O contacto é obrigatório")
     @Pattern(regexp = "\\d{9}", message = "O contacto deve ter 9 dígitos")
     private String contact;
 
