ec71ace11e51f56710ec0dd0cda3cf1a33ad7f36
Navi <maria.korjenevskaya@ua.pt>
2026-04-01 17:13:18 +0100

    feat: Add Form and Candidatura entities

---
+++ candidaturas/src/main/java/pt/florinhas/candidaturas/domain/Candidatura.java
@@ -0,0 +1,36 @@
+package pt.florinhas.candidaturas.domain;
+
+// Java
+import java.time.Instant;
+import java.util.Map;
+
+// Mongo
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
+
+// Lombok
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AccessLevel;
+
+@Getter
+@Setter
+@Document(collection = "candidaturas")
+public class Candidatura {
+
+    @Id
+    @Setter(AccessLevel.NONE)
+    private String id;
+
+    private String formId; // ref. ao Form
+
+    private Map<String, Object> respostas;
+
+    private String estado;
+
+    private Long criadoPor;
+    private Instant criadoEm;
+    // Resubmissões
+    private Long atualizadoPor;
+    private Instant atualizadoEm;
+}
---
+++ candidaturas/src/main/java/pt/florinhas/candidaturas/domain/Form.java
@@ -0,0 +1,33 @@
+package pt.florinhas.candidaturas.domain;
+
+// Java
+import java.time.Instant;
+import java.util.Map;
+
+// Mongo
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
+
+// Lombok
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AccessLevel;
+
+@Getter
+@Setter
+@Document(collection = "forms")
+public class Form {
+    @Id
+    @Setter(AccessLevel.NONE) // Para não permitir mudanças do ID
+    private String id;
+    
+    private String name;
+
+    private Map<String, Object> schema;
+    private Map<String, Object> uiSchema;
+
+    private Long criadoPor;
+    private Instant criadoEm;
+    private Long atualizadoPor;
+    private Instant atualizadoEm;
+}
