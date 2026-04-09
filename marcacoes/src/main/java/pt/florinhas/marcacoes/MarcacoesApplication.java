d96057654083c7bc6fbee2408c557902dfd8bd92
Navi <ivanhoroshko68@ua.pt>
2026-04-09 20:06:16 +0100

    chore: update main with common_data repositories

--- marcacoes/src/main/java/pt/florinhas/marcacoes/MarcacoesApplication.java
+++ marcacoes/src/main/java/pt/florinhas/marcacoes/MarcacoesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -17,6 +18,10 @@ import pt.florinhas.common_data.repository.FuncionarioRepository;
 @EntityScan(basePackages = {
 		"pt.florinhas.marcacoes.domain",
 		"pt.florinhas.common_data.domain"
+})
+@EnableJpaRepositories(basePackages = {
+		"pt.florinhas.marcacoes.repository",
+		"pt.florinhas.common_data.repository"
 })
 @EnableScheduling
 public class MarcacoesApplication {
