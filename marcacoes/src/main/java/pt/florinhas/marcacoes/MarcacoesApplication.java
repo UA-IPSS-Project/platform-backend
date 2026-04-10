0a1a3dba8034fc55a5d9cbecde3387a068bbdf35
Navi <ivanhoroshko68@ua.pt>
2026-04-10 19:37:37 +0100

    chore: update marcacoes main to include common-data files

--- marcacoes/src/main/java/pt/florinhas/marcacoes/MarcacoesApplication.java
+++ marcacoes/src/main/java/pt/florinhas/marcacoes/MarcacoesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,6 +23,10 @@ import pt.florinhas.common_data.repository.FuncionarioRepository;
 @EnableJpaRepositories(basePackages = {
 		"pt.florinhas.marcacoes.repository",
 		"pt.florinhas.common_data.repository"
+})
+@ComponentScan(basePackages = {
+		"pt.florinhas.marcacoes",
+		"pt.florinhas.common_data"
 })
 @EnableScheduling
 public class MarcacoesApplication {
