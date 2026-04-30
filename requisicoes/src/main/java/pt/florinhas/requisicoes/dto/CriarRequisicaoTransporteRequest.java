e7cc26cfff62de8b1aa04de81222ffa2d9502b19
Danuny21 <daniel.f.nunes@ua.pt>
2026-04-30 12:07:45 +0100

    refactor: make transporteIds optional in CriarRequisicaoTransporteRequest DTO

--- requisicoes/src/main/java/pt/florinhas/requisicoes/dto/CriarRequisicaoTransporteRequest.java
+++ requisicoes/src/main/java/pt/florinhas/requisicoes/dto/CriarRequisicaoTransporteRequest.java
@@ -18,7 +18,7 @@ public record CriarRequisicaoTransporteRequest(
         @NotNull LocalDateTime dataHoraRegresso,
         @NotNull @PositiveOrZero Integer numeroPassageiros,
         @NotBlank String condutor,
-        @NotEmpty List<Long> transporteIds,
+        List<Long> transporteIds,
         @Deprecated
         Long transporteId,
         RequisicaoPeriodicaConfigRequest periodica) {
