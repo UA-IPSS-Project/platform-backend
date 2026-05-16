ec935d89af4c5228aded59a1a1eb80a18e181ddf
NunoLeitF <120404252+Nunoua112994@users.noreply.github.com>
2026-05-16 17:13:39 +0100

    fix

--- api-gateway/src/test/java/pt/florinhas/api_gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/pt/florinhas/api_gateway/ApiGatewayApplicationTests.java
@@ -1,13 +1,10 @@
 package pt.florinhas.api_gateway;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ApiGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
