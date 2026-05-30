1d4e3b26ed4832cb03fecb4744880f6432d43455
NunoLeitF <120404252+Nunoua112994@users.noreply.github.com>
2026-05-30 12:50:43 +0100

    tentativa de correção 2

--- notificacoes/src/test/java/pt/florinhas/notificacoes/NotificacoesApplicationTest.java
+++ notificacoes/src/test/java/pt/florinhas/notificacoes/NotificacoesApplicationTest.java
@@ -3,12 +3,15 @@ package pt.florinhas.notificacoes;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
+@ActiveProfiles("test")
 class NotificacoesApplicationTest {
 
     @Test
     void main_NaoDeveFalhar() {
-
         assertDoesNotThrow(() ->
                 NotificacoesApplication.main(
                         new String[] {}));
