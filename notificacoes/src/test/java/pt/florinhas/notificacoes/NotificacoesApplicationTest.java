adb103e765a454ead9f4543bbd4387bb7ec8f8db
NunoLeitF <120404252+Nunoua112994@users.noreply.github.com>
2026-05-30 12:46:18 +0100

    correção

--- notificacoes/src/test/java/pt/florinhas/notificacoes/NotificacoesApplicationTest.java
+++ notificacoes/src/test/java/pt/florinhas/notificacoes/NotificacoesApplicationTest.java
@@ -3,7 +3,6 @@ package pt.florinhas.notificacoes;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
 
 class NotificacoesApplicationTest {
 
